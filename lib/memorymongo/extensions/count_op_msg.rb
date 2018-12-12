7e3e93bd930acfe9146972a80448693fab0fab61
Vladislav Belov <v.belov@agileprofi.ru>
2018-12-12 15:32:32 +0300

    fix for mongoid_class.collection.estimated_document_count

--- lib/memorymongo/extensions/count_op_msg.rb
+++ lib/memorymongo/extensions/count_op_msg.rb
@@ -5,7 +5,7 @@ module MemoryMongo
 
       def dispatch_message_in_memory
         collection = memory_server.get_collection(req.db_name, req.selector['count'])
-        docs = collection.filter_documents(req.selector.query)
+        docs = collection.filter_documents(req.selector.query || {})
         FakeReply.new([{n: docs.count, ok: 1.0}])
       end
     end
